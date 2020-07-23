f10bb81206fe1582b5d41ebe791938b5a5197fc7
Daniel <hanakoscar@outlook.com>
2020-07-23 13:20:33 -0400

    step 10 and 11

--- src/components/SongTile.js
+++ src/components/SongTile.js
@@ -3,7 +3,7 @@ import React, { Fragment } from "react"
 const SongTile = props => {
   return (
     <Fragment>
-      <p>{props.songs[0].name} - {props.songs[0].artist}</p>
+      <p className={props.className} onClick={props.handleClick}>{props.name} - {props.artist}</p>
     </Fragment>
   )
 }
--- src/components/SonglistContainer.js
+++ src/components/SonglistContainer.js
@@ -1,10 +1,31 @@
-import React from "react"
+import React, { useState } from "react";
 import SongTile from "./SongTile"
+
 const SongListContainer = props => {
+  const [selectedSong, setSelectedSong] = useState(null);
+  const allTheSongs = props.songs.map(song => {
+    const handleClick = () => {
+      if (selectedSong === song.id) {
+        setSelectedSong(null);
+      } else {
+        setSelectedSong(song.id);
+      }
+    }
+    let className = (selectedSong === song.id) ? "selected" : "";
+  return(
+    <SongTile 
+      key={song.id}
+      name={song.name}
+      artist={song.artist}
+      handleClick={handleClick}
+      className={className}
+    />
+  )
+})
   return(
     <div className="columns small-6">
       <h1>Songs</h1>
-      <SongTile songs={props.songs}/>
+      {allTheSongs}
     </div>
   );
 };
